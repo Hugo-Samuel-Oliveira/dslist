4f8ebc72c713a34dea7c585254ef88c663ad3e43
Hugo Samuel <194290319+Hugo-Samuel-Oliveira@users.noreply.github.com>
2025-01-25 23:57:21 -0300

    SQL, projection, GET Games by list

--- src/main/java/com/hugo/dslist/dto/GameMinDTO.java
+++ src/main/java/com/hugo/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.hugo.dslist.dto;
 
 import com.hugo.dslist.entities.Game;
+import com.hugo.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -19,6 +20,14 @@ public class GameMinDTO {
         this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+        this.shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/hugo/dslist/services/GameService.java
+++ src/main/java/com/hugo/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.hugo.dslist.services;
 import com.hugo.dslist.dto.GameDTO;
 import com.hugo.dslist.dto.GameMinDTO;
 import com.hugo.dslist.entities.Game;
+import com.hugo.dslist.projections.GameMinProjection;
 import com.hugo.dslist.repositories.GameRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
